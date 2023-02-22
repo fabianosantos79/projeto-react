635e7ffc5168c4d7bb3c73f603ba83c6f62aea00
Fabiano <fabiano.ssantos79@gmail.com>
2023-02-22 03:52:25 -0300

    update system

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header";
 import { Photo } from "./components/Photo";
+import React, { useState } from "react";
 
 const App = () => {
 
@@ -8,9 +9,30 @@ const App = () => {
   let n1: number = 10;
   let n2: number = 3;
   let link: string = 'http://google.com'
+  let n: number = 0;
 
   function soma(n1:number, n2:number):number {
     return n1+n2;
+  }
+
+  const [nameUser, setNameUser] = useState('Fabiano');  
+  const [num, setNum] = useState(0);
+  const [nameInput, setNameInput] = useState('');
+
+  const minus = () => {
+    setNum(num - 1)
+  }
+
+  const more = () => {
+    setNum(num + 1)
+  }
+
+  const handleClickButton = () => {
+    setNameUser('Viviane');
+  }
+
+  const handleInputName = (event : React.ChangeEvent<HTMLInputElement>) => {
+    setNameInput(event.target.value);
   }
 
   return(
@@ -26,7 +48,25 @@ const App = () => {
       Olá, {`${name.toLocaleUpperCase()} ${lastName.toLowerCase()}`} tudo bem?<br />
       A Soma é: {soma(n1, n2) + n2}<br />
       O link do <a href={link}>Google</a>
+      <hr />
+      Nome:
+      <input type='text' onChange = { handleInputName } />
+      o nome dele é: { nameInput }
+      <hr />
+      <hr />
+      <hr />
+    Meu nome é {nameUser}
+    <br />
+      <button onClick={ handleClickButton }>
+        Clique aqui
+      </button>
+      <hr />
+      <hr />
+      <button onClick = {minus}>-</button>
+      <p>{num}</p>
+      <button onClick = {more}>+</button>
     </div>
+    
   )
 }
 
